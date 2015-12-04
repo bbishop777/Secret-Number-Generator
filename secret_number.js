e5407fe2150bd068afe9233d3e81629b695c746d
BBishop <brad.bishop68@gmail.com>
2015-12-03 19:58:46 -1000

    Completes Secret Number

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
-'use strict';
+  'use strict';
 module.exports = function() {
-  
+  var a = (Math.random() * 1000000) + 1;
+    if (a >=1000000) {
+      a = 1000000;
+    }
+      return function () {
+        return a;
+      };
 };
+
